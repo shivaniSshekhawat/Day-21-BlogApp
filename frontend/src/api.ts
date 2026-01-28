855162de17867a9b5392c3cfaccb0df4ea6d6644
shivaniSshekhawat <shivanishekhawat2694@gmail.com>
2026-01-29 01:25:21 +0530

    *base url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://day-21-blog-app-qw1l.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
